f78375c3d79180d608bed633800184ddf7023526
Yawen Wu <wuyawen1990@gmail.com>
2024-02-12 12:03:49 -0800

    update

---
+++ javascripts/extra.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2GNV2WLRYJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2GNV2WLRYJ');
+</script>
